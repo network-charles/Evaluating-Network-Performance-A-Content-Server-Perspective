1c943bc1d1fd6d9184cae23cdc81ba034df6f7c9
Charles Uneze <charlesniklaus@gmail.com>
2023-01-27 20:22:39 +0100

    Update python_IPWhois.py
    Edited the file so it is easily readable.

--- python_IPWhois.py
+++ python_IPWhois.py
@@ -1,54 +1,57 @@
-'''
-    1. This program uses regular expression to extract the IP addresses from the output of a traceroute command saved to a txt file
-       see .
-    2. Uses the Netaddr library to verify if the IP address is a public IPV4 address.
-    3. Makes a GET request to https://ipwhois.io/documentation#tabs-format to obtain the geolocation information of each ipv4 address.
-    4. Save each response to a JSON file and then an excel spreadsheet.
-    shout-out to https://github.com/network-charles/Traceroute-Data-to-IP-Geolocation-Data.
-'''
+
 import requests #HTTP request library to make API requests
 from netaddr import * #module for working with IP address
 import re  #regular expression (regex) module to manipulate strings
 import pandas as pd
 
-#declaring an empty list to store public IPV4 addresses
+-----------------------------------------------------------------------------------
+        An empty list to store public IPV4 addresses
+-----------------------------------------------------------------------------------
 public_ipv4_addresses  = []
 
-#opening the output of the traceroute saved to Google.txt
+-----------------------------------------------------------------------------------
+        		Opening the output of the traceroute data saved in .txt
+-----------------------------------------------------------------------------------
 with open('c:/Users/Admin/Documents/Project/file.txt', 'r') as file:
     for line in file:
+	-----------------------------------------------------------------------------------
+        A Regex that searches and captures all the IPv4 addresses inside the text file
+        -----------------------------------------------------------------------------------
         #shoutout to https://feralpacket.org/?p=817 for the IPV4 regex below
         ipv4_regex = re.compile(r'''(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}
                         (?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])''', re.VERBOSE)
 
-        #searching for IPV4 address in the traceroute result
+        
         ipv4_address = ipv4_regex.search(line)
 
-        #defining a condition to save found IPV4 address to a variable
+        -----------------------------------------------------------------------------------
+        	Defining a condition to save found IPV4 address to a variable.
+            Then the string IPV4 address found to actual IPV4 address using the Netaddr library.
+            Verify if the IP address is a public IP address and append to the public IPV4 list.
+	-----------------------------------------------------------------------------------
         if ipv4_address:
             ipv4_address = ipv4_address.group(0)
  
-            #converting the string IPV4 address found to actual IPV4 address using the Netaddr library
+            
             ipv4_address = IPAddress(ipv4_address)
             
-            #verify if the IP address is a public IP address and append to the public IPV4 list
-            #if ipv4_address.is_unicast() and not ipv4_address.is_private():
-            public_ipv4_addresses.append(ipv4_address)
-            #else:
-            #    continue
-    
+    ----------------------------------------------------------------------------------------
+   	Covert the public IPV4 address back to string format, so it can be added to the API url.
+    Store the API response as text.
+    Creating a file to save the text response of each API call to a JSON file. The 'a' options appends 	   each new response to the response to the end of the file.
+    Write the response for each API call to the JSON file on a new line.
+	----------------------------------------------------------------------------------------
     for idx, ipv4_address in enumerate(public_ipv4_addresses):
         #coverting the public IPV4 address back to string format
         ipv4_address = str(ipv4_address)
 
-        #making an API call to https://api.ipgeolocation.io/ipgeo?apiKey=API_KEY&ip=1.1.1.1&fields=geo to obtain geolocation information only.
         ip_geolocation_info = requests.get('http://ipwhois.pro/' + str(ipv4_address) + '?key=API_Key')
         
         #storing the API response as text
         geo_information = ip_geolocation_info.text
 
         #creating a file to save the text response of each API call to a JSON file. The 'a' options appends the response to the end of the file.
-        json_doc = open('c:/Users/Admin/Documents/Project/Traceroutes/Top 20 Sites by Alexa/Using MTN/IP Geolocators/Using IPWhois API/Excel data/1/1x001.json', 'a')
+        json_doc = open('c:/Users/Admin/Documents/Project/File.json', 'a')
 
         #writing the response for each API call to the JSON file on a new line
         json_doc.write(f'{geo_information}\n')
@@ -56,9 +59,11 @@ with open('c:/Users/Admin/Documents/Project/file.txt', 'r') as file:
         #closing the file
         json_doc.close()
 
-#using Pandas to read the JSON file
+-----------------------------------------------------------------
+        Using Pandas to read the JSON file
+        Converting the JSON file into an excel sheet
+------------------------------------------------------------------
 json_doc_to_excel = pd.read_json('c:/Users/Admin/Documents/Project/file.json', lines=True)
 
-#saving to excel sheet.
 json_doc_to_excel.to_excel('c:/Users/Admin/Documents/Project/file.xlsx', sheet_name='ip_geoinformation', index=False)
 
